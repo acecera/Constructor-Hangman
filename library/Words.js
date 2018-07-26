cea51949c431ab01b02611eebcc80073dcb589ec
Anthony Cera <ace_cera@yahoo.com>
2018-07-26 15:54:25 -0700

    exported allthe words for the game.

--- library/Words.js
+++ library/Words.js
@@ -1,33 +1,21 @@
-var Letter = require("./letter.js");
-
-function Word (value) {
-    this.value = value;
-    this.letters = [];
-    this.guessesMade = "";
-    for (var i = 0; i < this.value.length; i++) {
-        this.letters.push(new Letter.letter(this.value[i]));
-    }
-};    
-
-word.prototype.findLetter = function(letter) {
-    var lowerCaseLetter = letter.toLowerCase();
-    if (this.guessesMade.indexOf(lowerCaseLetter) != -1) {
-        return "You've guessed that letter already!";
-    }
-    this.guessesMade += lowerCaseLetter;
-    for (var i = 0; i < this.letters.length; i++) {
-        if(this.letter[i].value.toLowerCase() == lowerCaseLetter) {
-           this.letters[i].show = true;
-        }
-    }
-};
-
-word.prototype.toString = function(){
-    var output = "";
-    for (var i = 0; i < this.letters.length; i++) {
-        output += this.letters[i].printInfo();
-    }
-    return output;
-}
-
-module.exports = Word;
+module.exports = [
+    "Brasil",
+    "France", 
+    "Spain", 
+    "Italy", 
+    "Bolivia", 
+    "Argentina",
+    "England", 
+    "Belgium", 
+    "Germany", 
+    "England", 
+    "Poland", 
+    "Crotia", 
+    "Portugal", 
+    "Poland", 
+    "Sweden", 
+    "Japan", 
+    "Mexico",
+    "Uruguay",
+    "Russia"
+]
